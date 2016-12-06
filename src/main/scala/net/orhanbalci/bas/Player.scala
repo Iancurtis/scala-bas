f6d9ccb3050ffad7b08626117c51a2cbc577afee
orhan.balci <orhan.balci@foreks.com>
2016-12-06 14:11:09 +0300

    protocol buffers definition file added

--- src/main/scala/net/orhanbalci/bas/Player.scala
+++ src/main/scala/net/orhanbalci/bas/Player.scala
@@ -1,11 +1,14 @@
 package net.orhanbalci.bas
 import akka.actor.{Actor, ActorRef, Props, ActorLogging}
 import akka.io.{Tcp}
+import akka.util.ByteString
 
 class Player(id: String, connection: ActorRef) extends Actor with ActorLogging{
 
   def receive = {
-    case Tcp.Received =>
+    case Tcp.Received(data) =>
+      log.debug(s"$id says ${data.utf8String}")
+      connection ! Tcp.Write(ByteString.fromString("hello from bas"))
     case Tcp.PeerClosed =>
       context.parent ! Table.UnRegister(id)
       context stop self
