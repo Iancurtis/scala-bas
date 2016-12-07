e5b58fdc90aa7fffa6ca244c919a5962eabd0cde
orhan.balci <orhan.balci@foreks.com>
2016-12-07 10:19:49 +0300

    protocol buffer integration

--- src/main/scala/net/orhanbalci/bas/Player.scala
+++ src/main/scala/net/orhanbalci/bas/Player.scala
@@ -1,4 +1,5 @@
 package net.orhanbalci.bas
+import net.orhanbalci.bas.protocol._
 import akka.actor.{Actor, ActorRef, Props, ActorLogging}
 import akka.io.{Tcp}
 import akka.util.ByteString
@@ -7,7 +8,9 @@ class Player(id: String, connection: ActorRef) extends Actor with ActorLogging{
 
   def receive = {
     case Tcp.Received(data) =>
-      log.debug(s"$id says ${data.utf8String}")
+      //log.debug(s"$id says ${data.utf8String}")
+      val request = Player.decodeIncommingMessage(data)
+      log.debug(s"$id request type ${request.requestType} request message ${request.textMessage}")
       connection ! Tcp.Write(ByteString.fromString("hello from bas"))
     case Tcp.PeerClosed =>
       context.parent ! Table.UnRegister(id)
@@ -18,7 +21,12 @@ class Player(id: String, connection: ActorRef) extends Actor with ActorLogging{
 
 object Player {
   def props(id: String, connection: ActorRef): Props = {
-    Props(new Player(id, connection))
+    Props(new Player(id, connection)) 
+  }
+
+  def decodeIncommingMessage(data : ByteString) : BasRequestResponse ={
+  	val dataArray = data.toArray
+  	BasRequestResponse.parseFrom(dataArray)
   }
 
 }
