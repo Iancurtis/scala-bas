2238c3c6be434e868d283e85dbc6f2de6b10b762
orhanbalci <orhanbalci@gmail.com>
2016-12-14 23:01:42 +0300

    more tests added

--- src/test/scala/net/orhanbalci/bas/TableSpec.scala
+++ src/test/scala/net/orhanbalci/bas/TableSpec.scala
@@ -1,17 +1,25 @@
 package net.orhanbalci.bascompile
 
-import akka.actor.ActorSystem
-import akka.testkit.{TestActorRef, TestKit}
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
 import org.scalatest.{MustMatchers, WordSpecLike}
 import net.orhanbalci.bas.Table
+import net.orhanbalci.bas.Room
 
-class TableSpec extends TestKit(ActorSystem("table-test-system")) with WordSpecLike with MustMatchers{
+class TableSpec extends TestKit(ActorSystem("table-test-system")) with WordSpecLike with MustMatchers with ImplicitSender{
   "A Table actor " must {
-    val actorRef = TestActorRef[Table]
+    val testParent = TestProbe()
+    val firstTable = TestActorRef[Table]
+    val secondTable = testParent.childActorOf(Props[Table])
     val connection = TestActorRef[Table]
     "register a player " in {
-      actorRef ! Table.Register("my player", connection)
-      actorRef.underlyingActor.players.size must equal(1)
+      firstTable ! Table.Register("my player", connection)
+      firstTable.underlyingActor.players.size must equal(1)
+    }
+
+    "update player count of table when registering " in {
+      secondTable ! Table.Register("test player", connection)
+      testParent.expectMsg(Room.UpdatePlayerCount(1))
     }
   }
 }
